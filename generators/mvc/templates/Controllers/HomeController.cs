f34f7d067d7fe6663c2951a546fc2e9d3f3cc8f1
Matt Rutledge <mattrutledge@believegroup.com>
2017-12-15 16:12:05 -0700

    Add Home page for start of module information and controller.

---
+++ generators/mvc/templates/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿/*
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+*/
+using DotNetNuke.Web.Mvc.Framework.Controllers;
+using DotNetNuke.Collections;
+using System.Web.Mvc;
+using DotNetNuke.Security;
+using DotNetNuke.Web.Mvc.Framework.ActionFilters;
+
+namespace <%= namespace%>.Modules.<%= moduleName %>.Controllers
+{
+    [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
+    [DnnHandleError]
+    public class HomeController : DnnController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [ModuleAction(ControlKey = "Edit", TitleKey = "AddItem")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Edit()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
