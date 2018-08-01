74dae87f7cf78f7098113383160158d2a0879ce2
Matt Rutledge <mrutledge@believegroup.com>
2018-08-01 16:18:09 -0600

    Update template

--- generators/personabar/templates/src/components/App.tsx
+++ generators/personabar/templates/src/components/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import PersonaBarPageHeader from "dnn-persona-bar-page-header";
 import PersonaBarPage from "dnn-persona-bar-page";
+import PersonaBarPageBody from "dnn-persona-bar-page-body";
 import Hello from './Hello.tsx';
 import TimerApp from './TimerApp.tsx';
 
@@ -10,17 +11,19 @@ interface AppProps {
 
 class App extends React.Component<AppProps, {}> {
     public static defaultProps: Partial<AppProps> = {
-        title: "<%= moduleName %>"
+        title: "Pbtest"
     };
 
     render() {
         return (
             <div>
                 <PersonaBarPage isOpen="true">
-                    <PersonaBarPageHeader title="{this.props.title}">
+                    <PersonaBarPageHeader title={this.props.title}>
                     </PersonaBarPageHeader>
-					<Hello name="I am a react persona bar module!" />
-                    <TimerApp />
+                    <PersonaBarPageBody>
+					    <Hello name="I am a react persona bar module!" />
+                        <TimerApp />
+                    </PersonaBarPageBody>
                 </PersonaBarPage>
             </div>
         );
