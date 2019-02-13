9d9b8218a0d96a81bf3b7e84bd8d91ab6781b4ba
Matt Rutledge <mtrutledge@gmail.com>
2019-02-13 15:20:12 -0700

    Add resx to spa tsx project

--- generators/spa/templates/ReactJS/tsx/src/app.tsx
+++ generators/spa/templates/ReactJS/tsx/src/app.tsx
@@ -1,6 +1,7 @@
 ﻿import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Hello from "./components/Hello";
+import resx from "localization";
 
 ReactDOM.render(
 	<div className="row">
