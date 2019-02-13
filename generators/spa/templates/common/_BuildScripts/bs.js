5774a4c7d8782581fb3c99a19c8d8017da7b5f18
Matt Rutledge <mtrutledge@gmail.com>
2019-02-13 12:56:12 -0700

    Add watch and browser-sync to spa module

---
+++ generators/spa/templates/common/_BuildScripts/bs.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+const pkg = require("../package.json");
+const path = require("path");
+let browserSync = require("browser-sync").create();
+
+browserSync.init({
+    proxy: pkg.dnn.localhost,
+    files: ["dist/**"],
+    serveStatic: ["dist"],
+    rewriteRules: [
+        {
+            match: new RegExp("/DesktopModules/<%= namespace %>/<%= moduleName %>/Resources/scripts/app-bundle.js"),
+            fn: function () {
+                return "/Resources/scripts/app-bundle.js";
+            }
+        },
+        {
+            match: new RegExp("/DesktopModules/<%= namespace %>/<%= moduleName %>/Resources/scripts/edit-bundle.js"),
+            fn: function () {
+                return "/Resources/scripts/edit-bundle.js";
+            }
+        },
+        {
+            match: new RegExp("/DesktopModules/<%= namespace %>/<%= moduleName %>/Resources/scripts/settings-bundle.js"),
+            fn: function () {
+                return "/Resources/scripts/settings-bundle.js";
+            }
+        }
+    ]
+});
---
+++ generators/spa/templates/common/_BuildScripts/watch.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-console */
+const pkg = require('../package.json');
+const fs = require('fs-extra');
+const path = require('path');
+const chokidar = require('chokidar');
+
+function copy(srcDir, srcRelativePath, destDir) {
+  const fullSrcPath = path.join(srcDir, srcRelativePath);
+  const fullDestPath = path.join(destDir, path.basename(srcRelativePath));
+
+  return fs
+    .ensureDir(path.dirname(fullSrcPath))
+    .then(() => fs.copy(fullSrcPath, fullDestPath));
+}
+
+const relative = srcFullPath => path.relative(srcDir, srcFullPath);
+
+let srcDir = 'dist/**';
+let destAssemblyDir = `${pkg.dnn.dnnRoot}/bin/`;
+// eslint-disable-next-line prettier/prettier
+let destModuleDir = `${pkg.dnn.dnnRoot}/DesktopModules/<%= namespace %>/<%= moduleName %>/`;
+
+const moduleWatcher = chokidar.watch(srcDir, {
+  ignoreInitial: true,
+  ignored: ['*.sql', '*.pdb', '*.dll']
+});
+
+moduleWatcher.on('add', path => {
+  console.log(relative(path));
+  copy(srcDir, relative(path), destModuleDir)
+    .then(() => console.log({ relative: relative(path), type: 'add' }))
+    .catch(reason => console.log(reason));
+});
+
+moduleWatcher.on('change', path => {
+  copy(srcDir, relative(path), destModuleDir)
+    .then(() => console.log({ relative: relative(path), type: 'change' }))
+    .catch(reason => console.log(reason));
+});
+
+const assemblyWatcher = chokidar.watch(srcDir, {
+  ignoreInitial: true,
+  ignored: ['!**/*.dll', '!**/*.pdb', '**']
+});
+
+assemblyWatcher.on('add', path => {
+  console.log(relative(path));
+  copy(srcDir, relative(path), destAssemblyDir)
+    .then(() => console.log({ relative: relative(path), type: 'add' }))
+    .catch(reason => console.log(reason));
+});
+
+assemblyWatcher.on('change', path => {
+  copy(srcDir, relative(path), destAssemblyDir)
+    .then(() => console.log({ relative: relative(path), type: 'change' }))
+    .catch(reason => console.log(reason));
+});
